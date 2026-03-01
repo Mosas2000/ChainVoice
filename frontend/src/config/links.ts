f52472b92be01c99a23a207d900e6e68bb2e251f
0xMosas <tosinolosasa@gmail.com>
2026-03-01 14:16:01 +0100

    add stacks explorer, hiro, and wallet links to config

--- frontend/src/config/links.ts
+++ frontend/src/config/links.ts
@@ -5,4 +5,8 @@
 export const EXTERNAL_LINKS = {
   github: 'https://github.com/Mosas2000/ChainVoice',
   stacksDocs: 'https://docs.stacks.co',
+  stacksExplorer: 'https://explorer.hiro.so',
+  hiroPlatform: 'https://www.hiro.so',
+  leatherWallet: 'https://leather.io',
+  xverseWallet: 'https://www.xverse.app',
 } as const;
